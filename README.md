3ad4bd79aa2ac858c1571a32446112038652d085
Aloïs Micard <a.micard@vold.lu>
2023-09-03 18:33:38 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # ghost-backup
-Homemade bash script to backup a Ghost instance
+
+Homemade bash script to back up a Ghost instance on S3.
+
+## Usage
+
+Using it is as simple as running:
+
+```bash
+$ ./ghost-backup /var/www/blog.example.org/
+```
+
+## Requirements
+
+### Runtime
+
+In order to work properly this script must be run as the same user who owns the Ghost instance.
+
+### Packages
+
+This script require the following binaries to be installed.
+
+- jq (provided by the [jq](https://tracker.debian.org/pkg/jq) package)
+- aws (provided by [awscli](https://tracker.debian.org/pkg/awscli) package)
+- mysqldump (provided by [mariadb-client](https://tracker.debian.org/pkg/mysql-client) package)
+
+## Configuration
+
+In order to be able to upload to S3 one must configure ghost-backup and aws cli. 
+
+### awscli
+
+The base aws cli configuration can be done by following [the official guide](https://docs.aws.amazon.com/cli/latest/reference/configure/).
+
+### ghost-backup
+
+In order to specify the s3 endpoint url and bucket to use one can create a configuration file for ghost-backup.
+
+The configuration file must be located at `~/.config/ghost-backup.json`.
+
+Here is an example configuration file:
+
+```json
+{
+  "/var/www/blog.example.org/": {
+    "aws": {
+      "endpoint": "https://s3.us-west-000.backblazeb2.com",
+      "bucket": "blog-example-org"
+    }
+  }
+}
+```
---
+++ ghost-backup.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+# Base variables
+INSTALL_PATH="$1"
+
+BACKUP_ROOT_PATH="$HOME/.ghost-backups"
+
+GHOST_CMD="/usr/local/bin/ghost"
+TAR_CMD="/usr/bin/tar"
+MYSQLDUMP_CMD="/usr/bin/mysqldump"
+MKDIR_CMD="/usr/bin/mkdir"
+JQ_CMD="/usr/bin/jq"
+AWS_CMD="/usr/bin/aws"
+RM_CMD="/usr/bin/rm"
+
+# Bootstrap base directories
+$MKDIR_CMD -p "$BACKUP_ROOT_PATH"
+
+# 1. Stop ghost instance
+$GHOST_CMD stop --dir "$INSTALL_PATH"
+
+# 2. backup file system
+$TAR_CMD cf "$BACKUP_ROOT_PATH/content.tar.gz" -C "$INSTALL_PATH" .
+
+# 3. backup MySQL database
+MYSQL_USER=$($JQ_CMD -r .database.connection.user "$INSTALL_PATH/config.production.json")
+MYSQL_PASSWORD=$($JQ_CMD -r .database.connection.password "$INSTALL_PATH/config.production.json")
+MYSQL_DATABASE=$($JQ_CMD -r .database.connection.database "$INSTALL_PATH/config.production.json")
+
+$MYSQLDUMP_CMD -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" > "$BACKUP_ROOT_PATH/database.sql"
+
+# 4. compress everything together
+BACKUP_FILENAME="backup-$(date +"%Y-%m-%d").tar.gz"
+$RM_CMD -f "$BACKUP_ROOT_PATH/$BACKUP_FILENAME"
+
+$TAR_CMD czf "$BACKUP_ROOT_PATH/$BACKUP_FILENAME" -C "$BACKUP_ROOT_PATH" content.tar.gz database.sql
+rm "$BACKUP_ROOT_PATH/content.tar.gz" "$BACKUP_ROOT_PATH/database.sql"
+
+# 5. upload to S3
+AWS_ENDPOINT_URL=$($JQ_CMD --arg BASE_CONFIG_KEY "$INSTALL_PATH" -r ".[\$BASE_CONFIG_KEY].aws.endpoint" "$BACKUP_ROOT_PATH/config.json")
+AWS_BUCKET=$($JQ_CMD --arg BASE_CONFIG_KEY "$INSTALL_PATH" -r ".[\$BASE_CONFIG_KEY].aws.bucket" "$BACKUP_ROOT_PATH/config.json")
+
+$AWS_CMD s3 --endpoint-url "$AWS_ENDPOINT_URL" cp "$BACKUP_ROOT_PATH/$BACKUP_FILENAME" "s3://$AWS_BUCKET/$BACKUP_FILENAME"
+
+# 6. restart ghost instance
+$GHOST_CMD start --dir "$INSTALL_PATH"
